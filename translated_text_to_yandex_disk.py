9e3de22bb535e60d4f46b3439aa485d65d7f27f2
daniil <shishovdanil@gmail.com>
2020-04-07 08:42:36 +0300

    initial commit

---
+++ translated_text_to_yandex_disk.py
@@ -0,0 +1,28 @@
+import requests
+import config
+
+token = config.disk_token
+URL = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+
+
+def to_disk(file_path):
+    params = {
+        'path': file_path,
+        'overwrite': True
+    }
+    headers = {
+        'Authorization': token
+    }
+    response = requests.get(URL, params=params, headers=headers)
+    # print(response.headers)
+    json_ = response.json()
+    # print(json_)
+    resp = requests.put(json_['href'])
+    if resp.status_code == 201:
+        print('Файл успешно загружен')
+    else:
+        print('Произошла ошибка при загрузке файла')
+
+
+if __name__ == '__main__':
+    to_disk('translated.txt')
